631cd9dd351c2ae23c38dd67a8a07908f9e6b145
Matt Madison <matt@madison.systems>
2019-04-13 10:24:19 -0700

    Add support for direct S3 mirrors using boto3

---
+++ layers/meta-testdistro/lib/oeaws/botos3fetcher.py
@@ -0,0 +1,37 @@
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+"""
+BitBake fetcher for AWS S3 using boto3
+
+Makes direct boto3 API calls for S3 fetches for improved
+performance over the built-in awscli-based fetcher.
+
+You must have the boto3 and botocore packages, and their
+dependencies, installed prior to use.
+
+
+Copyright (c) 2019, Matthew Madison <matt@madison.systems>
+"""
+
+import os
+import bb
+
+class S3(bb.fetch2.s3.S3):
+
+    def __init__(self, urls=None):
+        super().__init__(urls)
+        self.session = oeaws.s3session.S3Session()
+
+    def checkstatus(self, fetch, ud, d):
+        return self.session.get_object_info(ud.host, ud.path[1:]) is not None
+
+    def download(self, ud, d):
+        return self.session.download(ud.host, ud.path[1:], ud.localpath)
+
+try:
+    import oeaws.s3session
+    bb.fetch2.methods = [m for m in bb.fetch2.methods if not isinstance(m, bb.fetch2.s3.S3)] + [S3()]
+    bb.debug(1, "botos3fetcher: installed")
+except:
+    bb.debug(1, "botos3fetcher: s3session import failed")
+    pass
