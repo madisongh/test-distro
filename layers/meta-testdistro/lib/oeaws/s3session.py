29bc705a317b5a4b5c4ebcb2de0982cbf30550f0
Matt Madison <matt@madison.systems>
2020-05-09 07:24:13 -0700

    lib/oeaws: general s3 fetcher improvements
    - pass through environment variables to allow for profile
      selection and other settings
    - rework session setup to allow for profile selection
    - improve handling of instance metadata

--- layers/meta-testdistro/lib/oeaws/s3session.py
+++ layers/meta-testdistro/lib/oeaws/s3session.py
@@ -25,20 +25,25 @@ def s3retry_wait(trynumber):
 class S3Session(object):
     def __init__(self, maxtries=10):
         self.s3client = None
-        self.maxtries = maxtries
+        default_maxtries = os.getenv('AWS_METADATA_SERVICE_NUM_ATTEMPTS')
+        if default_maxtries is not None and int(default_maxtries) > maxtries:
+            self.maxtries = int(default_maxtries)
+        else:
+            self.maxtries = maxtries
+        default_timeout = os.getenv('AWS_METADATA_SERVICE_TIMEOUT')
+        if default_timeout is not None and int(default_timeout) > 1:
+            self.metadata_timeout = int(default_timeout)
+        else:
+            self.metadata_timeout = 10
 
     def makeclient(self):
+        os.environ['AWS_METADATA_SERVICE_NUM_ATTEMPTS'] = '{}'.format(self.maxtries)
+        os.environ['AWS_METADATA_SERVICE_TIMEOUT'] = '{}'.format(self.metadata_timeout)
         session = botocore.session.get_session()
-        timeout = session.get_config_variable('metadata_service_timeout')
-        if timeout < 10:
-            bb.debug(1, "increasing metadata service timeout to 10 seconds")
-            session.set_config_variable('metadata_service_timeout', 10)
-        retries = session.get_config_variable('metadata_service_num_attempts')
-        if retries < self.maxtries:
-            bb.debug(1, "increasing metadata service retries to %d" % self.maxtries)
-            session.set_config_variable('metadata_service_num_attempts', self.maxtries)
-        session.get_component('credential_provider').get_provider('assume-role').cache = botocore.credentials.JSONFileCache()
         self.s3client = boto3.Session(botocore_session=session).client('s3')
+        provider = session.get_component('credential_provider').get_provider('assume-role')
+        provider.cache = botocore.credentials.JSONFileCache()
+        bb.debug(1, "Using AWS profile: %s" % provider._profile_name)
 
     def upload(self, Filename, Bucket, Key):
         if self.s3client is None:
@@ -62,12 +67,13 @@ class S3Session(object):
             self.makeclient()
         for attempt in range(10):
             try:
+                bb.debug(2, "%s/%s: attempt %d" % (Bucket, Key, attempt))
                 info = self.s3client.head_object(Bucket=Bucket, Key=Key)
                 self.s3client.download_file(Bucket=Bucket, Key=Key, Filename=Filename)
                 if 'LastModified' in info:
                     mtime = int(time.mktime(info['LastModified'].timetuple()))
                     os.utime(Filename, (mtime, mtime))
-            except botocore.exceptions.NoCredentialsError:
+            except (botocore.exceptions.NoCredentialsError, botocore.exceptions.EndpointConnectionError):
                 s3retry_wait(attempt)
                 continue
             except botocore.exceptions.ClientError as e:
@@ -82,6 +88,7 @@ class S3Session(object):
                     pass
                 bb.warn("os.utime({}): {} (errno {})".format(Filename, e.strerror, e.errno))
                 return False
+            bb.debug(1, "{}/{}: success".format(Bucket, Key))
             return True
         bb.warn("{}/{}: credentials error on download for 10 attempts".format(Bucket, Key))
         return False
